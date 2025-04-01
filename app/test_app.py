38e60a2e6f5b16da1e10f2b5aab16db78b22b229
przemeksokoleoko <59530781+przemeksokoleoko@users.noreply.github.com>
2025-04-01 20:56:06 +0000

    implementing sherlock

--- app/test_app.py
+++ app/test_app.py
@@ -2,22 +2,37 @@ import pytest
 
 from run import app as application
 
+
 @pytest.fixture()
 def app():
     application.config.update({
-        'TESTING': True
-        })
+        "TESTING": True,
+    })
     yield application
 
-@pytest.fixture()
-def client(app):
+
+@pytest.fixture
+def client(app):   
     return app.test_client()
+
 
 @pytest.fixture()
 def runner(app):
     return app.test_cli_runner()
 
-def test_app(client):
-    response = client.get('/')
+
+def test_api(client):
+    response = client.get("/api/v1/movies/recommend")
     assert response.status_code == 200
-    assert b'Flask' in response.data
+    assert response.is_json
+    assert response.get_json()[0]["title"] != ""
+
+    response = client.get("/api/v1/movies/recommend?title=Kingpin")
+    assert response.status_code == 200
+    assert response.is_json
+    assert len(response.get_json()) >= 2
+
+    response = client.get("/api/v1/movies/recommend?title=Lost%20in%20Translation")
+    assert response.status_code == 200
+    assert response.is_json
+    assert len(response.get_json()) >= 5
